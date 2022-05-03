78f192963393bb097723fcc1137faf43a1e34f23
Tim <virtualxself@gmail.com>
2022-05-04 01:23:38 +0200

    добавил пару страниц

--- scripts/project.js
+++ scripts/project.js
@@ -1,5 +1,8 @@
 const lightbox = GLightbox({
   touchNavigation: true,
   loop: true,
-  autoplayVideos: true
+  autoplayVideos: true,
+  openEffect: 'none',
+  closeEffect: 'none',
+  slideEffect: 'none',
 });
